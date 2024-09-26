95c25f0a1764415fd0f42a73e77f9ef582c8faab
_NachoCampo <ignaciocampo19@gmail.com>
2024-09-26 14:11:43 -0300

    Campo-Verde
    Se hacen modificaciones para Gonzalo de este ticket: #43281 y también algunas modificaciones en consultas de Precios solicitadas por Rocío

--- Nacho/[Artículos-Precios] Impresión de Etiquetas/[Query] Generación de Etiquetas - Marcos.sql
+++ Nacho/[Artículos-Precios] Impresión de Etiquetas/[Query] Generación de Etiquetas - Marcos.sql
@@ -1,12 +1,14 @@
 --Se arma esta query para que @Marcos pueda armar su programa a medida para la generación de Etiquetas en Campo Verde
 
 /*Esta consulta es el buscador del programa donde se busca por Cod. Barra o Descripción del Artículo*/
--- Columnas que trae: 
+-- *******Columnas que trae: 
 -- * Descripción Artículo
 -- * Codigo de Barras 
 -- * Precio
 -- * Fecha de Modificación del Precio
--- ** Se filtra por los que son "Tipo no controla"
+-- * Si esta "Impreso" = S o N
+-- ******Filtros:
+-- ** Se filtra por los que son "Tipo no controla" y los "Unitarios"
 -- ** Se filtra por Lista de Precios = '10'
 -- ** Se agrega la búsqueda "Descripción" y "Codigo de Barras" para que el usuario pueda buscar por algún dato puntual e imprimir esa etiqueta.
 -- **Filtro por rango de fecha de modificación del precio.
@@ -19,12 +21,11 @@ SELECT
   CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_SI)[1]', 'nvarchar(max)') AS [IMPRESO]
 FROM STA11
 JOIN GVA17 ON STA11.COD_ARTICU = GVA17.COD_ARTICU -- JOIN con la tabla de precios
-WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') = 'TIPO NO CONTROLA' -- Filtro para solo traer los "Tipo No Controla"
-AND STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_SI)[1]', 'NVARCHAR(MAX)') = 'N' -- Filtro para solo traer los Etiquetas NO Impresas
+WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') in ('TIPO NO CONTROLA', 'TIPO PLU N si es UNITARIO') -- Filtro para solo traer los "Tipo No Controla" y "Si es Unitario"
 AND GVA17.NRO_DE_LIS = '10' -- Filtra por Lista de Precios = '10'
 AND (
   (STA11.DESCRIPCIO LIKE '%%' OR STA11.COD_BARRA LIKE '%%') -- Filtro para buscar por descripción o código de barras
-  OR GVA17.FECHA_MODI BETWEEN '' AND '' -- Filtro por rango de fecha de modificación del precio (como ejemplo)
+  OR GVA17.FECHA_MODI = '' -- Filtro por rango de fecha de modificación del precio (como ejemplo)
 );
 
 
@@ -35,31 +36,24 @@ AND (
 -- * Codigo de Barras 
 -- * Precio
 -- * Fecha de Modificación del Precio
--- ** Se filtra por los que son "Tipo no controla".
+-- * Si esta impreso = S o N
+-- ******Filtros:
+-- ** Se filtra por los que son "Tipo no controla" y "Unitarios"
 -- ** Se filtra por Lista de Precios = '10'.
--- ** Se trae los Precios qye aún no fueron impresos.
+-- ** Se trae los Precios que aún no fueron impresos.
 SELECT
   STA11.COD_ARTICU AS [CÓDIGO DE ARTÍCULO],
   STA11.DESCRIPCIO AS [DESCRIPCIÓN ARTÍCULO], -- Descripción del artículo
   STA11.COD_BARRA AS [CÓDIGO DE BARRAS], -- Código de Barras
-  REPLACE(CONVERT(VARCHAR, CAST(GVA17.PRECIO AS DECIMAL(18, 2)), 1), '.', ',') AS [PRECIO], -- Precio > Formatea el precio sin separador de miles
+  REPLACE(CONVERT(VARCHAR, CAST(GVA17.PRECIO AS INT), 1), '.', ',') AS [PRECIO], -- Precio > Formatea el precio sin separador de miles
   CONVERT(VARCHAR, GVA17.FECHA_MODI, 103) AS [FECHA MODIFICACIÓN PRECIO], -- Fecha de Modificación del Precio en formato DD/MM/YYYY
   CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_SI)[1]', 'nvarchar(max)') AS [IMPRESO] --Lee la columna "Impreso" de la STA11
 FROM STA11
 JOIN GVA17 ON STA11.COD_ARTICU = GVA17.COD_ARTICU -- JOIN con la tabla de precios
-WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') = 'TIPO NO CONTROLA' --Filtro para solo traer los "Tipo No Controla"
-AND STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_SI)[1]', 'NVARCHAR(MAX)') = 'N'; --Filtro para solo traer los Etiquetas Impresa
+WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') in ('TIPO NO CONTROLA', 'TIPO PLU N si es UNITARIO') --Filtro para solo traer los "Tipo No Controla" y "Si es Unitario"
+AND STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_SI)[1]', 'NVARCHAR(MAX)') = 'N' --Filtro para solo traer los Etiquetas Impresa
 AND GVA17.NRO_DE_LIS = '10' -- Filtra por Lista de Precios = '10'
 
-
-
-/*Este UPDATE es el que usa Marcos para cambiar al "Imprimir" la etiqueta el campo en el ABM del Artículo pero cambia toda la estructura del "Campo Adicional"*/
-Update STA11 set CAMPOS_ADICIONALES = 
-'<CAMPOS_ADICIONALES>
-  <CA_INFORMACION_PARA_BALANZA>TIPO NO CONTROLA </CA_INFORMACION_PARA_BALANZA>
-  <CA_SI>N</CA_SI>
-</CAMPOS_ADICIONALES>'
-WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') = 'TIPO NO CONTROLA' AND COD_ARTICU = ''
 
 /*Este UPDATE es el que usa Marcos para cambiar al "Imprimir" la etiqueta el campo en el ABM del Artículo pero cambia solamente la estructura que nos interesa a nosotros*/
 Update STA11 SET CAMPOS_ADICIONALES = 
@@ -68,4 +62,4 @@ Update STA11 SET CAMPOS_ADICIONALES =
             '<CA_SI>S</CA_SI>', 
             '<CA_SI>N</CA_SI>'
         ) AS XML)
-WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') = 'TIPO NO CONTROLA' AND COD_ARTICU = ''
+WHERE STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') in ('TIPO NO CONTROLA', 'TIPO PLU N si es UNITARIO') AND COD_ARTICU = ''
--- Nacho/[Artículos-Precios] Impresión de Etiquetas/[Trigger] TRG_UpdatePrice.sql
+++ Nacho/[Artículos-Precios] Impresión de Etiquetas/[Trigger] TRG_UpdatePrice.sql
@@ -1,30 +1,4 @@
 /*Este trigger se incluye por un desarrollo que pidieron para la impresión de etiquetas. Lo que hace es ante un cambio de precio en la Lista 10, se desmarca el tilde de "IMPRESIÓN ETIQUETA" de 'S' a 'N'*/
---Esta versión cambia el valor de toda la estructura de Campo Adicional de "IMPRESIÓN ETIQUETA".
-CREATE TRIGGER TRG_UpdatePrice
-ON GVA17
-AFTER UPDATE
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    -- Actualizar el campo <CA_SI> a 'N' en STA11 cuando se modifique el precio en GVA17
-    UPDATE STA11
-    SET CAMPOS_ADICIONALES = 
-        '<CAMPOS_ADICIONALES>
-            <CA_INFORMACION_PARA_BALANZA>TIPO NO CONTROLA </CA_INFORMACION_PARA_BALANZA>
-            <CA_SI>N</CA_SI>
-        </CAMPOS_ADICIONALES>'
-    FROM STA11 WITH (NOLOCK)
-    INNER JOIN inserted i WITH (NOLOCK) ON STA11.COD_ARTICU = i.COD_ARTICU
-    INNER JOIN deleted d WITH (NOLOCK) ON i.COD_ARTICU = d.COD_ARTICU
-    WHERE i.PRECIO <> d.PRECIO
-    AND STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') = 'TIPO NO CONTROLA'
-    AND i.NRO_DE_LIS = '10';
-END;
-
-
-
-/*Este trigger se incluye por un desarrollo que pidieron para la impresión de etiquetas. Lo que hace es ante un cambio de precio en la Lista 10, se desmarca el tilde de "IMPRESIÓN ETIQUETA" de 'S' a 'N'*/
 --Esta versión solamente cambia el valor del Campo Adicional de "IMPRESIÓN ETIQUETA" y no toda la estructura.
 CREATE TRIGGER TRG_UpdatePrice
 ON GVA17
@@ -45,7 +19,7 @@ BEGIN
     INNER JOIN inserted i WITH (NOLOCK) ON STA11.COD_ARTICU = i.COD_ARTICU
     INNER JOIN deleted d WITH (NOLOCK) ON i.COD_ARTICU = d.COD_ARTICU
     WHERE i.PRECIO <> d.PRECIO
-    AND STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') = 'TIPO NO CONTROLA'
+    AND STA11.CAMPOS_ADICIONALES.value('(/CAMPOS_ADICIONALES/CA_INFORMACION_PARA_BALANZA)[1]', 'NVARCHAR(MAX)') in ('TIPO NO CONTROLA', 'TIPO PLU N si es UNITARIO')
     AND i.NRO_DE_LIS = '10';
 END;
 
